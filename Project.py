ccb8834d65c8770a65281de465f04163fe5ef8bb
Angelo184 <74747342+Angelo184@users.noreply.github.com>
2020-12-07 17:29:24 +0800

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,198 @@
+BSCpE = {
+    "1": ["ARTS", "3.0", "Torres", "1:30PM - 3:00PM, Mon and Thu", "N/A"],
+    "2": ["BES241", "2.0", "Ruiz", "7:30AM - 10:30AM, Tue and Fri", "N/A"],
+    "3": ["CHEMENG", "3.0", "Jacinto", "9:00AM - 10:30AM, Mon and Thu", "N/A"],
+    "4": ["CHEMLAB", "1.0", "Jacinto", "10:30AM - 1:30PM, Wed", "N/A"],
+    "5": ["CL1A", "3.0", "Mendoza", "7:30AM - 9:00AM, Mon and Thu", "N/A"],
+    "6": ["COMP101", "1.0", "Dela Cruz", "7:30PM - 10:30PM, Wed", "N/A"],
+    "7": ["CPE111", "1.0", "Manalo", "12:00PM - 1:00PM, Thu", "N/A"],
+    "8": ["DIFCALC", "3.0", "Soriano", "4:30PM - 6:00PM, Tue and Fri", "N/A"],
+    "9": ["NSTP1", "3.0", "Averilla", "1:30PM - 3:00PM, Tue and Fri", "N/A"],
+    "10": ["PED1", "2.0", "Bienvenido", "3:00PM - 4:00PM, Tue and Fri", "N/A"]
+}
+BSECE = {
+    "1": ["BES112", "1.0", "De Los Reyes", "7:30AM - 10:30AM, Wed", "N/A"],
+    "2": ["BES241", "2.0", "Ruiz", "7:30AM - 10:30AM, Tue and Fri", "N/A"],
+    "3": ["CHEMENG", "3.0", "Jacinto", "9:00AM - 10:30AM, Mon and Thu", "N/A"],
+    "4": ["CHEMLAB", "1.0", "Jacinto", "10:30AM - 1:30PM, Wed", "N/A"],
+    "5": ["CL1A", "3.0", "Mendoza", "7:30AM - 9:00AM, Mon and Thu", "N/A"],
+    "6": ["DIFCALC", "3.0", "Soriano", "4:30PM - 6:00PM, Tue and Fri", "N/A"],
+    "7": ["NSTP1", "3.0", "Averilla", "1:30PM - 3:00PM, Tue and Fri", "N/A"],
+    "8": ["PED1", "2.0", "Bienvenido", "3:00PM - 4:00PM, Tue and Fri", "N/A"],
+    "9": ["PRBSTAT", "3.0", "Soriano", "3:00PM - 4:30PM, Mon and Thu", "N/A"],
+    "10": ["TECH", "3.0", "Gonzales", "12:00PM - 1:30PM, Mon and Thu", "N/A"]
+}
+BSME = {
+    "1": ["ARTS", "3.0", "Torres", "1:30PM - 3:00PM, Mon and Thu", "N/A"],
+    "2": ["SHOPRAC", "1.0", "Delos Reyes", "8:00AM - 11:00AM, Tue", "N/A"],
+    "3": ["CHEMENG", "3.0", "Jacinto", "9:00AM - 10:30AM, Wed and Thu ", "N/A"],
+    "4": ["CHEMLAB", "1.0", "Jacinto", "10:30AM - 1:30PM, Fri", "N/A"],
+    "5": ["CL1A", "3.0", "Mendoza", "4:00PM - 5:30PM, Mon and Thu", "N/A"],
+    "6": ["MATH", "3.0", "Soriano", "10:30AM - 12:00PM, Mon and Thu", "N/A"],
+    "7": ["ENGDRW", "1.0", "Delos Reyes", "8:00AM - 11:00AM, Fri", "N/A"],
+    "8": ["DIFCALC", "3.0", "Soriano", "4:00PM - 6:00PM, Tue and Fri", "N/A"],
+    "9": ["NSTP1", "3.0", "Averilla", "1:30PM - 3:00PM, Tue and Fri", "N/A"],
+    "10": ["PED1", "2.0", "Bienvenido", "4:00PM - 5:00PM, Tue and Fri", "N/A"]
+}
+BSCS = {
+    "1": ["ARTS", "3.0", "Torres", "1:30PM - 3:00PM, Mon and Thu", "N/A"],
+    "2": ["ITE101", "3.0", "Barcenas", "7:30AM - 10:30AM, Tue", "N/A"],
+    "3": ["ITE102", "3.0", "Dala", "9:00AM - 11:00AM, Mon", "N/A"],
+    "4": ["CHEMLAB", "1.0", "Jacinto", "10:30AM - 1:30PM, Wed", "N/A"],
+    "5": ["CL1A", "3.0", "Mendoza", "2:00PM - 3:00PM, Mon and Thu", "N/A"],
+    "6": ["NSTP1", "3.0", "Averilla", "1:30PM - 3:00PM, Fri", "N/A"],
+    "7": ["PED1", "2.0", "Bienvenido", "3:00PM - 4:00PM, Tue and Fri", "N/A"],
+    "8": ["TECH", "3.0", "Dela Cruz", "12:00PM - 1:30PM, Mon and Thu", "N/A"]
+}
+BSICT = {
+    "1": ["TECH", "3.0", "Dela Cruz", "12:00PM - 1:30PM, Mon and Thu", "N/A"],
+    "2": ["CL1A", "3.0", "Mendoza", "4:00PM - 5:30PM, Mon and Thu", "N/A"],
+    "3": ["IT200", "3.0", "Katigbak", "1:30PM - 3:00PM, Mon and Thu", "N/A"],
+    "4": ["ITE201", "3.0", "Bautista", "7:30AM - 10:30, Wed and Sat", "N/A"],
+    "5": ["ITE102", "3.0", "Dala-Ocenar", "9:00AM - 12:00PM, Mon and Thu", "N/A"],
+    "6": ["NSTP1", "3.0", "Averilla", "1:30PM - 3:00PM, Tue and Fri", "N/A"],
+    "7": ["PED1", "2.0", "Bienvenido", "3:00PM - 4:00PM, Tue and Fri", "N/A"]
+}
+BSEntrep = {
+    "1": ["MGMNT1", "3.0", "Mendoza", "12:00PM - 1:30PM, Mon and Thu", "N/A"],
+    "2": ["ENTRE1", "3.0", "Mendoza", "9:00AM - 10:30AM, Mon and Thu", "N/A"],
+    "3": ["CL1A", "3.0", "Mendoza", "7:30AM - 9:00AM, Mon and Thu", "N/A"],
+    "4": ["MICECO1", "3.0", "Chua", "9:00AM - 10:30AM, Tue and Fri", "N/A"],
+    "5": ["ACCTG1", "3.0", "Macapagal", "10:30AM - 12:00PM, Tue and Fri", "N/A"],
+    "6": ["NSTP1", "3.0", "Averilla", "1:30PM - 3:00PM, Tue and Fri", "N/A"],
+    "7": ["PED1", "2.0", "Bienvenido", "4:00PM - 5:30PM, Tue and Fri", "N/A"]
+}
+BSArchi = {
+    "1": ["ARD1", "2.0", "Chua", "9:30AM - 11:30AM, Mon and Thu", "N/A"],
+    "2": ["PR1", "3.0", "TORRES", "12:00PM - 1:30PM, Mon and Thu", "N/A"],
+    "3": ["TOA1", "2.0", "Ledda", "7:30AM - 11:30AM, Wed", "N/A"],
+    "4": ["CL1A", "3.0", "Mendoza", "4:00PM - 5:30PM, Mon and Thu", "N/A"],
+    "5": ["VT1", "2.0", "Yap", "7:30PM - 10:30PM, Wed", "N/A"],
+    "6": ["HOA1", "2.0", "Lustre", "2:00PM - 4:00PM, Thu", "N/A"],
+    "7": ["GR1", "3.0", "Serviño", "7:30AM - 11:00AM, Tue and Fri", "N/A"],
+    "8": ["NSTP1", "3.0", "Averilla", "1:30PM - 3:00PM, Tue and Fri", "N/A"],
+    "9": ["PED1", "2.0", "Bienvenido", "4:00PM - 5:00PM, Tue and Fri", "N/A"]
+}
+print("Good day esteemed student of Don Bosco Technical College!")
+def course_information(user):
+    while True:
+        if user == "YES":
+            dbtc_courses = ["Computer Engineering", "Electronics and Communication Engineering", "Mechanical Engineering",
+                        "Computer Science", "Information and Computer Technology", "Entrepreneurship", "Architecture"]
+            print("The available courses for the school year 2020-2021, 1st semester are the following:")
+            print(dbtc_courses)
+            student_program = (input("What is your course? \n")).upper()
+            print()
+            if student_program == "COMPUTER ENGINEERING":
+                print("{:10}{:10}{:15}{:35}{:15}".format("Courses", "Units", "Professor", "Schedule", "Room"))
+                for key, value in BSCpE.items():
+                    course, units, professor, schedule, room = value
+                    print("{:10}{:10}{:15}{:35}{:15}".format(course, units, professor, schedule, room))
+                print()
+                use = input("Would you like to view another course's details? \n"
+                            "Type 'YES' to view another course's details and 'NO' to exit program. \n").upper()
+                print()
+                if use == "NO":
+                    print("Thank you and have a great day!")
+                    break
+                else:
+                    print('Invalid Input! Please try again.')
+            elif student_program == "ELECTRONICS AND COMMUNICATION ENGINEERING":
+                print("{:10}{:10}{:15}{:35}{:15}".format("Courses", "Units", "Professor", "Schedule", "Room"))
+                for key, value in BSECE.items():
+                    course, units, professor, schedule, room = value
+                    print("{:10}{:10}{:15}{:35}{:15}".format(course, units, professor, schedule, room))
+                print()
+                use = input("Would you like to view another course's details? \n"
+                            "Type 'YES' to view another course's details and 'NO' to exit program. \n").upper()
+                print()
+                if use == "NO":
+                    print("Thank you and have a great day!")
+                    break
+                else:
+                    print('Invalid Input! Please try again.')
+            elif student_program == "MECHANICAL ENGINEERING":
+                print("{:10}{:10}{:15}{:35}{:15}".format("Courses", "Units", "Professor", "Schedule", "Room"))
+                for key, value in BSME.items():
+                    course, units, professor, schedule, room = value
+                    print("{:10}{:10}{:15}{:35}{:15}".format(course, units, professor, schedule, room))
+                print()
+                use = input("Would you like to view another course's details? \n"
+                            "Type 'YES' to view another course's details and 'NO' to exit program. \n").upper()
+                print()
+                if use == "NO":
+                    print("Thank you and have a great day!")
+                    break
+                else:
+                    print('Invalid Input! Please try again.')
+            elif student_program == "COMPUTER SCIENCE":
+                print("{:10}{:10}{:15}{:35}{:15}".format("Courses", "Units", "Professor", "Schedule", "Room"))
+                for key, value in BSCS.items():
+                    course, units, professor, schedule, room = value
+                    print("{:10}{:10}{:15}{:35}{:15}".format(course, units, professor, schedule, room))
+                print()
+                use = input("Would you like to view another course's details? \n"
+                            "Type 'YES' to view another course's details and 'NO' to exit program. \n").upper()
+                print()
+                if use == "NO":
+                    print("Thank you and have a great day!")
+                    break
+                else:
+                    print('Invalid Input! Please try again.')
+            elif student_program == "INFORMATION AND COMPUTER TECHNOLOGY":
+                print("{:10}{:10}{:15}{:35}{:15}".format("Courses", "Units", "Professor", "Schedule", "Room"))
+                for key, value in BSICT.items():
+                    course, units, professor, schedule, room = value
+                    print("{:10}{:10}{:15}{:35}{:15}".format(course, units, professor, schedule, room))
+                print()
+                use = input("Would you like to view another course's details? \n"
+                            "Type 'YES' to view another course's details and 'NO' to exit program. \n").upper()
+                print()
+                if use == "NO":
+                    print("Thank you and have a great day!")
+                    break
+                else:
+                    print('Invalid Input! Please try again.')
+            elif student_program == "ENTREPRENEUR":
+                print("{:10}{:10}{:15}{:35}{:15}".format("Courses", "Units", "Professor", "Schedule", "Room"))
+                for key, value in BSEntrep.items():
+                    course, units, professor, schedule, room = value
+                    print("{:10}{:10}{:15}{:35}{:15}".format(course, units, professor, schedule, room))
+                print()
+                use = input("Would you like to view another course's details? \n"
+                            "Type 'YES' to view another course's details and 'NO' to exit program. \n").upper()
+                print()
+                if use == "NO":
+                    print("Thank you and have a great day!")
+                    break
+                else:
+                    print('Invalid Input! Please try again.')
+            elif student_program == "ARCHITECTURE":
+                print("{:10}{:10}{:15}{:35}{:15}".format("Courses", "Units", "Professor", "Schedule", "Room"))
+                for key, value in BSArchi.items():
+                    course, units, professor, schedule, room = value
+                    print("{:10}{:10}{:15}{:35}{:15}".format(course, units, professor, schedule, room))
+                print()
+                use = input("Would you like to view another course's details? \n"
+                            "Type 'YES' to view another course's details and 'NO' to exit program. \n").upper()
+                print()
+                if use == "NO":
+                    print("Thank you and have a great day!")
+                    break
+                else:
+                    print('Invalid Input! Please try again.')
+            elif student_program == "EXIT":
+                print("Thank you and have a great day!")
+                break
+            else:
+                print("Invalid input. Please try again. If you wish to end the program type 'EXIT'")
+        elif user == "NO":
+            print("Thank you and have a great day!")
+            break
+        else:
+            print()
+            print("Invalid input. Type 'YES' to view course details and 'NO' to exit program")
+            user = input().upper()
+            print()
+course_information(input("Would you like to view your course information?""\n"
+                         "Type 'YES' to view course details and 'NO' to exit program""\n").upper())
